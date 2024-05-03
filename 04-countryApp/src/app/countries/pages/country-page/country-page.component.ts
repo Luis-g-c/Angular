3ad7f239aa33aead5ed988c13e8653cdb910a343
Luis Garcia <gcluis@outlook.es>
2024-05-03 17:23:42 +0200

    6.1

---
+++ 04-countryApp/src/app/countries/pages/country-page/country-page.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CountriesService } from '../../services/countries.service';
+import { switchMap } from 'rxjs';
+import { Country } from '../../interfaces/country';
+
+@Component({
+  selector: 'app-country-page',
+  templateUrl: './country-page.component.html',
+  styles: ``
+})
+export class CountryPageComponent implements OnInit{
+
+  public country?: Country;
+
+  constructor(
+    private activaredRoute: ActivatedRoute,
+    private router: Router,
+    private countriesService: CountriesService,
+
+  ) {}
+
+  ngOnInit(): void {
+    this.activaredRoute.params
+      .pipe(
+        switchMap( ({ id }) => this.countriesService.searchCountryByAlphaCode( id ) ),
+      )
+      .subscribe( country => {
+
+        if ( !country ) return this.router.navigateByUrl('');
+
+        return this.country = country;
+
+      });
+  }
+
+}
